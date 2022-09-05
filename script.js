99c41fed5b3692fcbdff40af8549e6dfa1926400
xXxINFARKTxXx <v.getmantsev@yandex.ru>
2022-09-05 23:22:59 +0300

    added section and easy animations

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let button = document.querySelector(`[data-button]`);
 let header = document.querySelector(`[data-header]`);
 let miaInfo = document.querySelector(`[data-mia-info]`);
 let leeInfo = document.querySelector(`[data-lee-info]`);
-console.log(miaInfo, leeInfo);
 
 button.addEventListener(`click`, () => {
   header.classList.toggle(`active`);
@@ -17,9 +16,27 @@ window.addEventListener(`resize`, () => {
 const showInfo = function (node) {
   node.style.visibility = `visible`;
   node.style.opacity = `1`;
-}
+};
 
 const hideInfo = function (node) {
   node.style.visibility = `invisible`;
   node.style.opacity = `0`;
+};
+
+let imgContainer = document.querySelector(".nft-slideshow");
+const fragment = document.createDocumentFragment();
+
+for (let i = 0; i < 3; i++) {
+  for (let i = 1; i < 10; i++) {
+    let img = document.createElement("img");
+    img.src = `img/main/nft-slideshow/nft (${i}).png`;
+    img.classList.add("nft-pic");
+    fragment.appendChild(img);
+  }
 }
+
+const enscaleImg = function (node, power) {
+  node.style.transform = `scale(${power})`;
+};
+
+imgContainer.appendChild(fragment);
