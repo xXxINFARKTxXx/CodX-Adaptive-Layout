cd546661918c6a6a62250f27362c2734d1379e73
xXxINFARKTxXx <v.getmantsev@yandex.ru>
2022-09-07 23:29:09 +0300

    finished slideshow section development

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function hideInfo (node) {
 // welcome ending
 
 // nftSlideshow beginning 
-let imgContainer = document.querySelector(`.nft-slideshow`);
+let slideshow = document.querySelector(`.slideshow`);
 const fragment = document.createDocumentFragment();
 
 const enscaleImg = function (i, j, power) {
@@ -38,22 +38,18 @@ const enscaleImg = function (i, j, power) {
 for (let i = 0; i < 3; i++) {
   for (let j = 1; j < 10; j++) {
     let img = document.createElement(`img`);
+    img.src = `img/main/slideshow/nft (${j}).png`;
+    img.classList.add(`slideshow__img`);
 
-    img.src = `img/main/nft-slideshow/nft (${j}).png`;
-    img.classList.add(`nft-pic`);
+    let imgWrap = document.createElement(`div`);
+    imgWrap.classList.add(`slideshow__img-container`);
+    imgWrap.style.backgroundImage = `url=(img/main/slideshow/nft (${j}).png)`;
 
-    // почему то работает только без "@keyframes slide", а почему непонятно
-    img.addEventListener(`mouseover`, () => {
-      img.style.transform = `scale(2)`;
-      img.style.zIndex = `2`;
-    });
-    img.addEventListener(`mouseout`, () => {
-      img.style.transform = `scale(1)`;
-      img.style.zIndex = `1`;
-    });
-
-    imgContainer.appendChild(img);
+    imgWrap.append(img);
+    slideshow.appendChild(imgWrap);
   }
 }
 // nftSlideshow ending
+
+
 
